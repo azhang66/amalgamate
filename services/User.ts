287627352e7915b916cc45e449eab67e1d9f6b11
Albert Z <albertzhang66@outlook.com>
2017-01-07 21:36:16 -0600

    feat(users): remove change functions

--- services/User.ts
+++ services/User.ts
@@ -5,24 +5,4 @@ export class User {
     username: string;
     password: string;
     class_period: number;
-
-    changeFirstName(first_name: string) {
-        return;
-    };
-
-    changeLastName(first_name: string) {
-        return;
-    };
-
-    changeUsername(first_name: string) {
-        return;
-    };
-
-    changePassword(first_name: string) {
-        return;
-    };
-
-    changeClassPeriod(first_name: string) {
-        return;
-    };
 }
