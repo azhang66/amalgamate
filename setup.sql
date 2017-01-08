082f88cb2a3c46bb14e852e057c572c0255edcc3
Albert Z <albertzhang66@outlook.com>
2017-01-07 21:31:51 -0600

    feat(db): update sql setup to add is_admin

--- setup.sql
+++ setup.sql
@@ -11,11 +11,12 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     class_period INTEGER(1) NOT NULL,
     date_added DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    date_last_updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    date_last_updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE USER 'refactored-potato'@'localhost' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON *.* TO 'refactored-potato'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
-SELECT 'If there are no errors, proceed to ensure the \'mkpasswd\' command exists and that the username format is correct.' AS 'INFO';
+SELECT 'If there are no errors, proceed to ensure the \'mkpasswd\' command exists.' AS 'INFO';
